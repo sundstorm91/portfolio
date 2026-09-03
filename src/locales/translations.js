c39a30efbef6edc371db411fd4e4bd089109a707
Aleksandr <sundstorm91@gmail.com>
2026-09-03 10:54:32 +0700

    chore: add file translation for localization

---
+++ src/locales/translations.js
@@ -0,0 +1,34 @@
+export const translations = {
+  ru: {
+    header: {
+      title: 'Адександр',
+      subtitle: 'React-разработчик',
+    },
+    about: {
+      title: 'Обо мне',
+      text: 'Привет! Я Aleksandr — React-разработчик.',
+    },
+    projects: {
+      title: 'Проекты',
+    },
+    contacts: {
+      title: 'Контакты',
+    },
+  },
+  en: {
+    header: {
+      title: 'Aleksandr',
+      subtitle: 'React Developer',
+    },
+    about: {
+      title: 'About Me',
+      text: 'Hi! I\'m Aleksandr — React Developer',
+    },
+    projects: {
+      title: 'Projects',
+    },
+    contacts: {
+      title: 'Contacts',
+    },
+  },
+};
